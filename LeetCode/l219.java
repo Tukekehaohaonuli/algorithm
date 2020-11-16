bb0267da20f638154e9054d1c79f1fece147f68f
Tukekehaohaonuli <850580234@qq.com>
2020-11-16 10:03:40 +0800

    push

---
+++ LeetCode/l219.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+public class l219 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])&&Math.abs(map.get(nums[i])-i)<=k){
+                return true;
+            }else{
+                map.put(nums[i],i);
+            }
+        }
+        return false;
+    }
+}
--- LeetCode/l92.java
+++ LeetCode/l92.java
@@ -31,9 +31,9 @@ public class l92 {
     public static void main(String[] args){
         ListNode s1=new ListNode(3);
         ListNode s2=new ListNode(5);
-        ListNode s3=new ListNode(3);
-        ListNode s4=new ListNode(4);
-        ListNode s5=new ListNode(5);
+        //ListNode s3=new ListNode(3);
+       // ListNode s4=new ListNode(4);
+        //ListNode s5=new ListNode(5);
         s1.next=s2;
         //s2.next=s3;s3.next=s4;s4.next=s5;
         l92 l=new l92();
